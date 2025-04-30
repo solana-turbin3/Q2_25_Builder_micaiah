2812ce66a608f64816f8f75c4ef1b153c09b1de0
MicaiahReid <micaiahreid@gmail.com>
2025-04-30 10:42:29 -0400

    chore(tests): add utils for sending txs while avoiding websockets

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,8 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program, AnchorProvider, Wallet } from "@coral-xyz/anchor";
 import { InvestInSol } from "../target/types/invest_in_sol";
-import { Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram } from "@solana/web3.js";
-import { createMint, getOrCreateAssociatedTokenAccount, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, getAccount, TOKEN_2022_PROGRAM_ID } from "@solana/spl-token";
+import { Keypair, LAMPORTS_PER_SOL, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
+import { createMint, getOrCreateAssociatedTokenAccount, TOKEN_PROGRAM_ID, ASSOCIATED_TOKEN_PROGRAM_ID, getAccount, TOKEN_2022_PROGRAM_ID, getMint, createSetAuthorityInstruction, AuthorityType } from "@solana/spl-token";
 import { MPL_TOKEN_METADATA_PROGRAM_ID } from "@metaplex-foundation/mpl-token-metadata";
 
 export const CN_MINT_ADDRESS = new PublicKey("LdV45HahKVpiVMdTCr6QFU8gK6uUqHRycpuGHkctAsn");
@@ -53,11 +53,77 @@ export async function initializeProtocol(
     const [treasuryPda] = PublicKey.findProgramAddressSync([Buffer.from("treasury")], program.programId);
 
     const configInfo = await provider.connection.getAccountInfo(configPda);
+
     if (configInfo === null) {
+
+
+        // make sure our config is the authority for the mints
+        let cnMintAccount = await getMint(
+            provider.connection,
+            cnMintPk,
+            "confirmed",
+            TOKEN_PROGRAM_ID
+          );
+        if (cnMintAccount.mintAuthority.toBase58() !== configPda.toBase58()) {
+            const tx = new Transaction().add(
+                createSetAuthorityInstruction(
+                    cnMintPk,
+                    initializer.publicKey,
+                    AuthorityType.MintTokens,
+                    configPda,
+                    [],
+                    TOKEN_PROGRAM_ID,
+                ),
+            );
+            const sig = await provider.connection.sendTransaction(tx, [initializer], {skipPreflight: true});
+            await confirmTransaction(provider, sig);
+        }
+
+        let ptMintAccount = await getMint(
+            provider.connection,
+            ptMintPk,
+            "confirmed",
+            TOKEN_PROGRAM_ID
+          );
+        if (ptMintAccount.mintAuthority.toBase58() !== configPda.toBase58()) {
+            const tx = new Transaction().add(
+                createSetAuthorityInstruction(
+                    ptMintPk,
+                    initializer.publicKey,
+                    AuthorityType.MintTokens,
+                    configPda,
+                    [],
+                    TOKEN_PROGRAM_ID,
+                ),
+            );
+            const sig = await provider.connection.sendTransaction(tx, [initializer]);
+            await confirmTransaction(provider, sig);
+        }
+
+        let collectionMintAccount = await getMint(
+            provider.connection,
+            collectionMintPk,
+            "confirmed",
+            TOKEN_PROGRAM_ID
+          );
+        if (collectionMintAccount.mintAuthority.toBase58() !== configPda.toBase58()) {
+            const tx = new Transaction().add(
+                createSetAuthorityInstruction(
+                    collectionMintPk,
+                    initializer.publicKey,
+                    AuthorityType.MintTokens,
+                    configPda,
+                    [],
+                    TOKEN_PROGRAM_ID,
+                ),
+            );
+            const sig = await provider.connection.sendTransaction(tx, [initializer]);
+            await confirmTransaction(provider, sig);
+        }
+
         console.log(`initializing protocol (Config: ${configPda.toBase58()})...`);
-        await program.methods
-            .initialize(optionDurationSeconds)
-            .accounts({
+        const tx = await program.methods.initialize(optionDurationSeconds)
+            .accountsStrict({
                 initializer: initializer.publicKey,
                 cnMint: cnMintPk,
                 ptMint: ptMintPk,
@@ -65,11 +131,15 @@ export async function initializeProtocol(
                 config: configPda,
                 treasury: treasuryPda,
                 systemProgram: SystemProgram.programId,
-                tokenProgram: TOKEN_2022_PROGRAM_ID,
+                tokenProgram: TOKEN_PROGRAM_ID,
             })
-            .signers([initializer]) // initializer needs to sign
-            .rpc({ commitment: "confirmed" });
-        console.log("protocol initialized.");
+            .transaction();
+        await sendAndConfirmTransaction(
+            provider,
+            tx,
+            initializer.publicKey,
+            [initializer],
+        );
     } else {
          console.log("protocol already initialized.");
     }
@@ -97,6 +167,37 @@ export function parseAnchorError(err: any): anchor.AnchorError | null {
      return null;
 }
 
+export async function sendAndConfirmTransaction(
+    provider: anchor.AnchorProvider,
+    transaction: anchor.web3.Transaction,
+    payer: PublicKey,
+    signers: Array<anchor.web3.Signer>,
+    options?: anchor.web3.ConfirmOptions) {
+        transaction.recentBlockhash = (await provider.connection.getLatestBlockhash()).blockhash;
+        transaction.feePayer = payer;
+        for (const signer of signers) {
+            transaction.partialSign(signer);
+        }
+        const sig = await provider.connection.sendRawTransaction(await transaction.serialize(), options);
+        await confirmTransaction(provider, sig);
+    }
+
+/**
+ * Helper to wait for a transaction to be confirmed. Used to avoid pinging surfpool over websockets.
+*/
+export async function confirmTransaction(provider: anchor.AnchorProvider, signature: string) {
+    let confirmed = false;
+    while (confirmed == false) {
+        let status = await provider.connection.getSignatureStatus(signature);
+        if (status.value.err) {
+            throw new Error(`Transaction failed: ${signature}, error: ${JSON.stringify(status.value.err)}`);
+        }
+        if (status.value.confirmationStatus == "confirmed" || status.value.confirmationStatus == "finalized") {
+            confirmed = true
+        }
+    }
+}
+
 /**
  * helper to request and confirm an airdrop.
  */
@@ -104,15 +205,9 @@ export async function requestAirdrop(provider: AnchorProvider, publicKey: Public
     try {
         console.log(`requesting ${lamports / LAMPORTS_PER_SOL} SOL for ${publicKey.toBase58()}...`);
         const signature = await provider.connection.requestAirdrop(publicKey, lamports);
-        const latestBlockhash = await provider.connection.getLatestBlockhash();
-        await provider.connection.confirmTransaction({
-            signature,
-            blockhash: latestBlockhash.blockhash,
-            lastValidBlockHeight: latestBlockhash.lastValidBlockHeight
-        }, "confirmed");
+        confirmTransaction(provider, signature);
         console.log(`airdrop confirmed for ${publicKey.toBase58()}.`);
     } catch (error) {
         console.error(`airdrop failed for ${publicKey.toBase58()}:`, error);
-        throw error;
     }
 }
